3b8259c5c1ca9d9d24f0b5f9038922fdf987e31f
“jhondominguez1” <jjjdomi@gmail.com>
2016-05-27 16:31:00 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+ El volumen de la unidad D es Particion_1
+ El n£mero de serie del volumen es: 0AAC-AFC3
+
+ Directorio de D:\xampplite\htdocs\dos_local
+
+27/05/2016  04:30 p. m.    <DIR>          .
+27/05/2016  04:30 p. m.    <DIR>          ..
+27/05/2016  04:30 p. m.                 0 index.php
+               1 archivos              0 bytes
+               2 dirs  58.395.279.360 bytes libres
